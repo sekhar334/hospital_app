3ea8f987294e516a7ff406ac57a2dcf966135da2
sekhar334 <137845373+sekhar334@users.noreply.github.com>
2023-12-20 09:08:46 +0530

    Update mongoose.js

--- Hospital-management-nodejs-master/server/db/mongoose.js
+++ Hospital-management-nodejs-master/server/db/mongoose.js
@@ -4,9 +4,6 @@ var mongoose = require ('mongoose');
 mongoose.Promise = global.Promise;
 
 //change the database with yours
-mongoose.connect("mongodb://admin:admin123@ds145220.mlab.com:45220/nhs-app", {
-  useNewUrlParser: true, // To avoid deprecation warning
-  useUnifiedTopology: true, // To use the new Server Discover and Monitoring engine
-});
+mongoose.connect("mongodb://admin:admin123@ds145220.mlab.com:45220/nhs-app")
 
 module.exports = {mongoose};
